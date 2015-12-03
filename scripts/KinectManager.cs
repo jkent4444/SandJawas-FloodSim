e37b4be42ffbed893605d71a2393344c94aa455c
john <jkent4444@gmail.com>
2015-12-03 23:14:13 +1000

    Initial push

---
+++ scripts/KinectManager.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Windows.Kinect;
+using UnityEngine.UI;
+using System;
+
+public class KinectManager : MonoBehaviour {
+
+	public bool Initalised = false;
+
+	private KinectSensor sensor;
+	private MultiSourceFrameReader reader;
+
+	public int DepthHeight { get; private set; }
+	public int DepthWidth { get; private set; }
+
+	private const int downsample = 4;
+	private const double depthScale = 0.1;
+
+	private ushort[] depthData;
+	private ushort[] zS;
+
+	public GameObject meshThing;
+
+	private Meshable meshable;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Initalised) {
+			bool depthProcessed = false;
+
+			if (reader != null)
+			{
+				MultiSourceFrame multiframe = reader.AcquireLatestFrame();
+				if (multiframe != null)
+				{
+					DepthFrame frame = multiframe.DepthFrameReference.AcquireFrame();
+					if (frame != null) {
+						frame.CopyFrameDataToArray(depthData);
+						frame.Dispose();
+						depthProcessed = true;
+
+						Debug.Log (depthData[10000]);
+					}
+				}
+
+				multiframe = null;
+				remesh();
+			}
+		}
+
+	}
+
+	public void StartKinect()
+	{
+		if (Initalised == false) {
+			Initalised = true;
+
+			sensor = KinectSensor.GetDefault();
+
+			if (sensor == null)
+			{
+				Initalised = false;
+				return;
+			}
+
+			reader = sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Depth);
+			DepthWidth = sensor.DepthFrameSource.FrameDescription.Width;
+			DepthHeight = sensor.DepthFrameSource.FrameDescription.Height;
+
+			depthData = new ushort[DepthWidth * DepthHeight];
+
+			if (!sensor.IsOpen){
+				sensor.Open();
+			}
+
+			zS = new ushort[(DepthHeight / downsample) * (DepthWidth / downsample)];
+
+			meshable = meshThing.GetComponent<Meshable>();
+			meshable.CreateMesh(DepthWidth/downsample, DepthHeight/downsample);
+		}
+	}
+
+	private void remesh()
+	{
+		for (int y = 0; y < DepthHeight; y+= downsample) {
+			for (int x = 0; x < DepthWidth; x+= downsample) {
+				int indexX = x/downsample;
+				int indexY = y/downsample;
+				int smallIndex = (indexY * (DepthWidth / downsample)) + indexX;
+
+				ushort avg = averageDepth(x,y);
+
+				zS[smallIndex] = avg;
+			}
+		}
+
+		/*for (int y = 1; y < DepthHeight / downsample -1; y++) {
+			for (int x = 1; x < DepthWidth / downsample - 1; x++) {
+
+			}
+		}*/
+
+		meshable.setZs (ref zS);
+	}
+
+	private ushort averageDepth(int x, int y)
+	{
+		uint sum = 0;
+
+		uint ignore = 0;
+
+		for (int y1 = y; y1 < y + 4; y1++) {
+			for (int x1 = x; x1 < x + 4; x1++) {
+				int fullIndex = (y1 * DepthWidth) + x1;
+
+				if (depthData[fullIndex] == 0 || depthData[fullIndex] > 4500)
+				{
+					sum += 4500;
+					ignore++;
+				} else {
+					sum += depthData[fullIndex];
+				}
+			}
+		}
+
+		if (ignore > 0 && ignore != 16) {
+			sum = sum - (ignore * 4500);
+			return Convert.ToUInt16 (sum / (16 - ignore));
+		} else if (ignore == 16) {
+			return 4500;
+		} else {
+			return Convert.ToUInt16(sum /16);
+		}
+	}
+}
---
+++ scripts/Meshable.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Meshable : MonoBehaviour {
+
+	private Mesh mesh;
+	private Vector3[] vertices;
+	private Vector2[] uvs;
+	private int[] triangles;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void CreateMesh(int width, int height)
+	{
+		mesh = new Mesh ();
+		this.GetComponent<MeshFilter> ().mesh = mesh;
+
+		vertices = new Vector3[width * height];
+		uvs = new Vector2[width * height];
+		triangles = new int[6 * ((width - 1) * (height - 1))];
+
+		int triIndex = 0;
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int index = (y * width) + x;
+
+				vertices[index] = new Vector3(x, -y, 0);
+				uvs[index] = new Vector2(((float)x / (float)width), ((float)y / (float)height));
+
+				if ((x != width - 1) && y != (height - 1)) {
+					int topLeft = index;
+					int topRight = topLeft + 1;
+					int bottomLeft = topLeft + width;
+					int bottomRight = bottomLeft + 1;
+
+					triangles[triIndex++] = topLeft;
+					triangles[triIndex++] = topRight;
+					triangles[triIndex++] = bottomLeft;
+					triangles[triIndex++] = bottomLeft;
+					triangles[triIndex++] = topRight;
+					triangles[triIndex++] = bottomRight;
+				}
+			}
+		}
+
+		mesh.vertices = vertices;
+		mesh.uv = uvs;
+		mesh.triangles = triangles;
+		mesh.RecalculateNormals ();
+		transform.position = new Vector3 (-width / 2, height / 2, 0);
+	}
+
+	public void setZs(ref ushort[] zS)
+	{
+		for (int i = 0; i < vertices.Length; i++) {
+			vertices[i].z = Convert.ToSingle (zS[i]) * 0.1f;
+
+		}
+		mesh.vertices = vertices;
+		mesh.uv = uvs;
+		mesh.triangles = triangles;
+		mesh.RecalculateNormals ();
+
+	}
+}
